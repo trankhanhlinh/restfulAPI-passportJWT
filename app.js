2573fbcbf5b1a798a13ffa7dcff1c4df0bb540d3
Trần Khánh Linh <trankhanhlinh98@gmail.com>
2019-10-22 23:45:22 +0700

    enable CORS

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
