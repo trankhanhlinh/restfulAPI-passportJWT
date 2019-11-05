b54ec776306938899e15710edea1cdcd5a8f64db
Trần Khánh Linh <trankhanhlinh98@gmail.com>
2019-11-06 02:36:51 +0700

    upate socket io

--- app.js
+++ app.js
@@ -8,22 +8,23 @@ var app = express();
 app.use(cors());
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
-io.origins((origin, callback) => {
-  if (origin !== 'https://restfulapi-passport-jwt.herokuapp.com') {
-    return callback('origin not allowed', false);
-  }
-  callback(null, true);
-});
+// var io = require('socket.io')(server, {
+//   handlePreflightRequest: (req, res) => {
+//     const headers = {
+//       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+//       'Access-Control-Allow-Origin': req.headers.origin, //or the specific origin you want to give access to,
+//       'Access-Control-Allow-Credentials': true
+//     };
+//     res.writeHead(200, headers);
+//     res.end();
+//   }
+// });
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// view engine middleware
-require('./middlewares/socket_io.middleware')(app);
 
 // view engine middleware
 require('./middlewares/view_engine.middleware')(app);
