285c49987f82793b5964454e1d6bdca64ef948ef
Linh Tran <tklinh@tma.com.vn>
2019-11-05 13:40:12 +0700

    update destination in multer

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 var multer = require('multer');
 var storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, '../public/images');
+    cb(null, './public/images');
   },
   filename: function(req, file, cb) {
     var filetype = '';
